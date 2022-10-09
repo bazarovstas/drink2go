24dcef0449a4e59cb91ae63de388f16f77da960b
Stanislav B <bazarovstas@gmail.com>
2022-10-09 11:05:15 +0300

    Дописана сборка и проверена; добавлена favicon; мелкие правки

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,99 @@ import sass from 'gulp-dart-sass';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
+import csso from 'postcss-csso';
+import del from 'del';
+import rename from 'gulp-rename';
+import htmlmin from 'gulp-htmlmin';
+import terser from 'gulp-terser';
+import squoosh from 'gulp-libsquoosh';
+import svgmin from 'gulp-svgmin';
+// import svgstore from 'gulp-svgstore';
+
+
+// Clean
+const clean = () => {
+  return del('build');
+};
+
+
+// Copy
+const copy = (done) => {
+  gulp.src([
+    'source/fonts/*.{woff2,woff}',
+    'source/*.ico',
+    'source/*.webmanifest',
+  ], {
+    base: 'source'
+  })
+    .pipe(gulp.dest('build'))
+  done();
+}
+
+
+// HTML
+const html = () => {
+  return gulp.src('source/*.html')
+  .pipe(htmlmin({ collapseWhitespace: true }))
+  .pipe(gulp.dest('build'));
+}
+
 
 // Styles
-
 export const styles = () => {
   return gulp.src('source/sass/style.scss', { sourcemaps: true })
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
+    // для тестирования сборки из папки source
+    // .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
 }
 
-// Server
 
+// Scripts
+const scripts = () => {
+  return gulp.src('source/js/main.js')
+  .pipe(terser())
+  .pipe(rename('main.min.js'))
+  .pipe(gulp.dest('build/js'));
+}
+
+
+// Images
+const optimizeImages = () => {
+  return gulp.src('source/images/**/*.{jpg,png}')
+  .pipe(squoosh())
+  .pipe(gulp.dest('build/images'));
+}
+
+
+const copyImages = () => {
+  return gulp.src('source/images/**/*.{jpg,png}')
+  .pipe(gulp.dest('build/images'));
+}
+
+
+// SVG
+const svg = () => {
+  return gulp.src(['source/images/**/*.svg', 'source/images/svg-css/*.svg', 'source/images/svg-inline/*.svg'])
+  .pipe(svgmin())
+  .pipe(gulp.dest('build/images'));
+}
+
+
+// Server
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
+      // для тестирования сборки из папки source
+      // baseDir: 'source'
     },
     cors: true,
     notify: false,
@@ -32,14 +105,41 @@ const server = (done) => {
   done();
 }
 
-// Watcher
 
+// Watcher
 const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
   gulp.watch('source/*.html').on('change', browser.reload);
+  gulp.watch('source/js/*.js', gulp.series(scripts));
 }
 
 
-export default gulp.series(
-  styles, server, watcher
+// Build
+export const build = gulp.series(
+  clean,
+  copy,
+  optimizeImages,
+  gulp.parallel(
+    html,
+    styles,
+    scripts,
+    svg,
+  )
 );
+
+
+// Source
+export default gulp.series(
+  clean,
+  copy,
+  copyImages,
+  gulp.parallel(
+    html,
+    styles,
+    scripts,
+    svg,
+  ),
+  gulp.series (
+    server,
+    watcher
+  ));
--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,20 @@
-import {mobileMenuToogle} from './mobile-menu.js';
+function mobileMenuToogle () {
+  let nav = document.querySelector('.main-header');
+  let toggle = document.querySelector('.main-nav-user__toogle');
 
-mobileMenuToogle();
+  nav.classList.remove('main-nav--nojs');
+  nav.classList.remove('main-nav--open');
+  nav.classList.add('main-nav--closed');
+
+  toggle.addEventListener('click', function () {
+    if (nav.classList.contains('main-nav--closed')) {
+      nav.classList.remove('main-nav--closed');
+      nav.classList.add('main-nav--open');
+    } else {
+      nav.classList.add('main-nav--closed');
+      nav.classList.remove('main-nav--open');
+    }
+  });
+}
+
+mobileMenuToogle ();
